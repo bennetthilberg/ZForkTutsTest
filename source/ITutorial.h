e51637f1b19d6fdb914d8b2298bbe923fd167388
Chloe Morscheck <chloemorscheck@Chloes-MacBook-Pro.local>
2022-03-01 13:29:53 -0600

    itut changes - tried adding third state

--- source/ITutorial.h
+++ source/ITutorial.h
@@ -20,24 +20,40 @@ class ITutorial {
         static void PrintComplete() { std::cout << "Tutorial Complete!" << std::endl; }
 
         ITutorial(UI::Document animation, UI::Document settings, UI::Document explanation, UI::Document learnmore, UI::Document buttons, UI::Canvas mycanvas): doc("emp_base"), my_button([](){}, "Start Tutorial"){
-            
+            // Probably want to do this in symAnimate 
             doc << my_button;
             my_button.SetCSS("position", "relative");
 
+            /* First state: No effects, trigger to second when button presed */
             tut.AddState("first_state");
+            //tut.AddOverlayEffect("first_state", buttons, "black", 0.5, -1, true);
+
+            /* Second state: from first, to third, overlay and popover*/
             tut.AddState("second_state");
+
+            // Popover remains into the third_state, while overlay does not
+            //tut.AddPopoverEffect("second_state", settings, "Welcome to Symbulation, an evolution modeling software.");
+            tut.AddOverlayEffect("second_state", buttons, "blue", 0.5, -1, true);
+            // Can we change the contents of the start tutorial button sp that it change to end tutorial once we have entered the tutorial
+            
+
+            /* third state */
+            tut.AddState("third_state");
+            tut.AddOverlayEffect("third_state", buttons, "yellow", 0.5, -1, true);
+
+            /* End state */
             tut.AddState("end_state", &PrintComplete);
+
+
+            /* Transitions */
             tut.AddEventListenerTrigger("first_state", "second_state", my_button, "click");
-            tut.AddEventListenerTrigger("second_state", "end_state", my_button, "click");
-            // tut.AddManualTrigger("second_state", "end_state", "enter_input_trigger");
+            tut.AddExistingTrigger("second_state", "third_state", "click");
+            tut.AddExistingTrigger("third_state", "end_state", "click");
 
-            tut.AddOverlayEffect("first_state", buttons, "black", 0.5, -1, true);
-            tut.AddOverlayEffect("second_state", buttons, "black", 0.5, -1, true);
-            tut.AddOverlayEffect("end_state", buttons, "black", 0.5,-1, true);
+            
+            
+            //tut.AddOverlayEffect("end_state", buttons, "black", 0.5,-1, true);
             // tut.AddCSSEffect("first_state", my_button, "z-index", "50000");
-
-            tut.AddPopoverEffect("second_state", buttons, "This is a pop over!!!!");
-
             // buttons.Button("toggle").SetCSS("position", "relative");
             // buttons.Button("toggle").SetCSS("z-index", "1");
             // my_button.SetCSS("z-index", "2");
@@ -47,19 +63,8 @@ class ITutorial {
         }
 
         void startTut(UI::Document animation, UI::Document settings, UI::Document explanation, UI::Document learnmore, UI::Document buttons, UI::Canvas mycanvas) {
-            // tut.AddState("first_state");
-            // tut.AddState("second_state");
-            // tut.AddState("end_state", &PrintComplete);
-            // tut.AddEventListenerTrigger("first_state", "second_state", my_button, "click");
-            // // tut.AddManualTrigger("second_state", "end_state", "enter_input_trigger");
 
-            // tut.AddOverlayEffect("first_state", buttons, "black", 0.5, 3, true);
-            // tut.AddOverlayEffect("second_state", buttons, "black", 0.5, 3, true);
-            // // tut.AddCSSEffect("first_state", my_button, "z-index", "50000");
-
-            // // tut.AddOverlayEffect("second_state", buttons, "green", 0.5, 1000, true);
-            // // tut.AddPopoverEffect("second_state", buttons, "This is a pop over!!!!");
-
+            
             buttons.Button("toggle").SetCSS("position", "relative");
             buttons.Button("toggle").SetCSS("z-index", "2");
             // // my_button.SetCSS("z-index", "2");
