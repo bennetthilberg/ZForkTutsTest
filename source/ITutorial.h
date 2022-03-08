b49664b9945368a19de2148129a4d32729bf16cf
Zhixing <jiangz@carleton.edu>
2022-03-07 23:12:22 -0600

    two popovers and one overlay

--- source/ITutorial.h
+++ source/ITutorial.h
@@ -15,52 +15,54 @@ class ITutorial {
         UI::Document doc;
         UI::Button my_button;
         Tutorial tut;
+        UI::Button start_but;
         bool started = false;
 
     public:
         static void PrintComplete() { std::cout << "Tutorial Complete!" << std::endl; }
 
         ITutorial(UI::Document animation, UI::Document settings, UI::Document explanation, UI::Document learnmore, UI::Document buttons, UI::Canvas mycanvas): doc("emp_base"), my_button([](){}, "Start Tutorial"){
-            // Probably want to do this in symAnimate 
             doc << my_button;
-            my_button.SetCSS("position", "relative");
+                my_button.SetCSS("position", "relative");
 
-            /* First state: No effects, trigger to second when button presed */
-            tut.AddState("first_state");
-            //tut.AddOverlayEffect("first_state", buttons, "black", 0.5, -1, true);
+                /* First state: No effects, trigger to second when button presed */
+                tut.AddState("first_state");
+                //tut.AddOverlayEffect("first_state", buttons, "black", 0.5, -1, true);
 
-            /* Second state: from first, to third, overlay and popover*/
-            tut.AddState("second_state");
+                /* Second state: from first, to third, overlay and popover*/
+                tut.AddState("second_state");
 
-            // Popover remains into the third_state, while overlay does not
-            tut.AddPopoverEffect("second_state", buttons, "Click it to start the experiement", "-2.4vh", "-2vw");
-            tut.AddPopoverEffect("second_state", buttons, "This is a walk through of the UI", "27vh", "-3vw");
-            // tut.AddOverlayEffect("second_state", buttons, "black", 0.8, 10, true);
-            // tut.AddCustomVisualEffect("second_state", animation, "z_index","13");
-            // Can we change the contents of the start tutorial button sp that it change to end tutorial once we have entered the tutorial
+
+                tut.AddOverlayEffect("second_state", buttons, "black", 0.8, 10, true);
+                // Popover remains into the third_state, while overlay does not
+                tut.AddPopoverEffect("second_state", buttons, "Click it to start the experiement", "-2.4vh", "-2vw");
+                tut.AddPopoverEffect("second_state", buttons, "This is a walk through of the UI", "27vh", "-3vw");
+
+
+     
+         
+
+            // /* third state */
+            // // tut.AddState("third_state");
+            // // tut.AddOverlayEffect("third_state", buttons, "yellow", 0.5, -1, true);
+
+            // /* End state */
+            // tut.AddState("end_state", &PrintComplete);
+
+
+            // /* Transitions */
+            // tut.AddEventListenerTrigger("first_state", "second_state", my_button, "click");
+            // tut.AddEventListenerTrigger("second_state", "third_state", my_button, "click");
+            // // tut.AddExistingTrigger("third_state", "end_state", "click");
+
             
-
-            /* third state */
-            // tut.AddState("third_state");
-            // tut.AddOverlayEffect("third_state", buttons, "yellow", 0.5, -1, true);
-
-            /* End state */
-            tut.AddState("end_state", &PrintComplete);
-
-
-            /* Transitions */
-            tut.AddEventListenerTrigger("first_state", "second_state", my_button, "click");
-            tut.AddEventListenerTrigger("second_state", "third_state", my_button, "click");
-            // tut.AddExistingTrigger("third_state", "end_state", "click");
-
             
-            
-            //tut.AddOverlayEffect("end_state", buttons, "black", 0.5,-1, true);
-            // tut.AddCSSEffect("first_state", my_button, "z-index", "50000");
-            // buttons.Button("toggle").SetCSS("position", "relative");
-            // buttons.Button("toggle").SetCSS("z-index", "1");
-            // my_button.SetCSS("z-index", "2");
-            tut.StartAtState("first_state");
+            // //tut.AddOverlayEffect("end_state", buttons, "black", 0.5,-1, true);
+            // // tut.AddCSSEffect("first_state", my_button, "z-index", "50000");
+            // // buttons.Button("toggle").SetCSS("position", "relative");
+            // // buttons.Button("toggle").SetCSS("z-index", "1");
+            // // my_button.SetCSS("z-index", "2");
+            // tut.StartAtState("first_state");
             started=true;
 
             
@@ -68,8 +70,30 @@ class ITutorial {
 
         void startTut(UI::Document animation, UI::Document settings, UI::Document explanation, UI::Document learnmore, UI::Document buttons, UI::Canvas mycanvas) {
             if (started) {
-                buttons.Button("toggle").SetCSS("position", "relative");
-                buttons.Button("toggle").SetCSS("z-index", "11");
+                /* third state */
+                tut.AddState("third_state");
+
+
+                /* End state */
+                tut.AddState("end_state", &PrintComplete);
+                start_but = buttons.Button("toggle");
+                std::cout << "start_but is: "<<start_but<<std::endl;
+                std::cout <<"my_button is: "<<my_button<<std::endl;
+                /* Transitions */
+                tut.AddEventListenerTrigger("first_state", "second_state", my_button, "click");
+                tut.AddEventListenerTrigger("second_state", "third_state", start_but, "click");
+                start_but.SetCSS("position", "relative");
+                start_but.SetCSS("z-index", "11");
+
+                
+                
+                //tut.AddOverlayEffect("end_state", buttons, "black", 0.5,-1, true);
+                // tut.AddCSSEffect("first_state", my_button, "z-index", "50000");
+                // buttons.Button("toggle").SetCSS("position", "relative");
+                // buttons.Button("toggle").SetCSS("z-index", "1");
+                // my_button.SetCSS("z-index", "2");
+                tut.StartAtState("first_state");
+                
 
             }
             
