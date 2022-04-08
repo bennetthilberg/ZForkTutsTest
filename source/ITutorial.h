32415759ccd840289cad7f4be3dee922d21ad9dc
Chloe Morscheck <chloemorscheck@Chloes-MacBook-Pro.local>
2022-04-08 15:48:45 -0500

    structure of tut in Itut

--- source/ITutorial.h
+++ source/ITutorial.h
@@ -24,69 +24,59 @@ class ITutorial {
         ITutorial(UI::Document animation, UI::Document settings, UI::Document explanation, UI::Document learnmore, UI::Document buttons, UI::Canvas mycanvas): doc("emp_base"), my_button([](){}, "Start Tutorial"), ok_but([](){}, "OK"){
             doc << my_button;
             my_button.SetCSS("position", "relative");
-
-            /* First state: No effects, trigger to second when button presed */
-            tut.AddState("first_state");
-            //tut.AddOverlayEffect("first_state", buttons, "black", 0.5, -1, true);
-
-            /* Second state: from first, to third, overlay and popover*/
-            tut.AddState("second_state");
-
-
-            tut.AddOverlayEffect("second_state", buttons, "black", 0.8, 10, true);
-            // Popover remains into the third_state, while overlay does not
-            tut.AddPopoverEffect("second_state", buttons, "This is a walk through of the UI", ok_but, "15vh", "-1vw");
-            tut.AddPopoverEffect("second_state", buttons, "Click it to start the experiement", ok_but, "-0.5vh", "-2vw");
-            // buttons << ok_but;
-
-
-    
-        
-
-            // /* third state */
-            tut.AddState("third_state");
-            // // tut.AddOverlayEffect("third_state", buttons, "yellow", 0.5, -1, true);
-            tut.AddState("fourth_state", &PrintComplete);
-
-            // /* End state */
-            tut.AddState("end_state", &PrintComplete);
-            tut.AddEventListenerTrigger("first_state", "second_state", my_button, "click", "click_trigger");
-            tut.AddExistingTrigger("second_state", "third_state", "click_trigger");
-
             ok_but.SetCSS("position", "relative");
 
-            // /* Transitions */
-            // tut.AddEventListenerTrigger("first_state", "second_state", my_button, "click");
-            // tut.AddEventListenerTrigger("second_state", "third_state", my_button, "click");
-            // // tut.AddExistingTrigger("third_state", "end_state", "click");
-
-            
-            
-            // //tut.AddOverlayEffect("end_state", buttons, "black", 0.5,-1, true);
-            // // tut.AddCSSEffect("first_state", my_button, "z-index", "50000");
-            // // buttons.Button("toggle").SetCSS("position", "relative");
-            // // buttons.Button("toggle").SetCSS("z-index", "1");
-            // // my_button.SetCSS("z-index", "2");
-            // tut.StartAtState("first_state");
-
+            /* State declarations */
+            tut.AddState("first_state");
+            tut.AddState("second_state");
+            tut.AddState("third_state");
+            tut.AddState("fourth_state");
+            tut.AddState("fifth_state");
+            tut.AddState("sixth_state");
+            tut.AddState("seventh_state");
+            tut.AddState("eighth_state");
+            tut.AddState("end_state", &PrintComplete);
         }
 
         void startTut(UI::Document animation, UI::Document settings, UI::Document explanation, UI::Document learnmore, UI::Document buttons, UI::Canvas mycanvas) {
             start_but = buttons.Button("toggle");
-    
+
             start_but.SetCSS("position", "relative");
             start_but.SetCSS("z-index", "11");
-            tut.AddEventListenerTrigger("third_state", "end_state", ok_but, "click", "click_ok");
-            // ok_but.SetCSS("top", "-0.5vh");
             ok_but.SetCSS("left", "0.7vw");
+
+            /* Transitions */
+            tut.AddEventListenerTrigger("first_state", "second_state", my_button, "click", "click_trigger");
+            tut.AddEventListenerTrigger("second_state", "third_state", ok_but, "click", "click_ok");
+            tut.AddExistingTrigger("third_state", "fourth_state", "click_ok");
+            tut.AddExistingTrigger("fourth_state", "fifth_state", "click_ok");
+            tut.AddExistingTrigger("fifth_state", "sixth_state", "click_ok");
+            tut.AddExistingTrigger("sixth_state", "seventh_state", "click_ok");
+            tut.AddExistingTrigger("seventh_state", "eighth_state", "click_ok");
+            tut.AddExistingTrigger("eighth_state", "end_state", "click_ok");
+            
+            /* Effects */
+            // First state: no effects
+            // Second state: gray overlay spotlight: animation, popover
+            tut.AddOverlayEffect("second_state", buttons, animation, "black", 0.8, 10, true);
+            tut.AddPopoverEffect("second_state", buttons, "This is a walk through of the UI", ok_but, "15vh", "-1vw");
+            tut.AddPopoverEffect("second_state", buttons, "Click it to start the experiement", ok_but, "-0.5vh", "-2vw");
+            // Third state: 
+            tut.AddOverlayEffect("third_state", buttons, animation, "yellow", 0.8, 10, true);
+            // Fourth state: 
+            tut.AddOverlayEffect("fourth_state", buttons, animation, "yellow", 0.8, 10, true);
+            // Fifth state: 
+            tut.AddOverlayEffect("fifth_state", buttons, animation, "yellow", 0.8, 10, true);
+            // Sixth state: 
+            tut.AddOverlayEffect("sixth_state", buttons, animation, "yellow", 0.8, 10, true);
+            // Seventh state: 
+            tut.AddOverlayEffect("seventh_state", buttons, animation, "yellow", 0.8, 10, true);
+            // Eighth state: 
+            tut.AddOverlayEffect("eighth_state", buttons, animation, "yellow", 0.8, 10, true);
+
+
             animation.SetCSS("position", "relative");
             animation.SetCSS("z-index", "12");
-                
-            //tut.AddOverlayEffect("end_state", buttons, "black", 0.5,-1, true);
-            // tut.AddCSSEffect("first_state", my_button, "z-index", "50000");
-            // buttons.Button("toggle").SetCSS("position", "relative");
-            // buttons.Button("toggle").SetCSS("z-index", "1");
-            // my_button.SetCSS("z-index", "2");
             tut.StartAtState("first_state");
                 
 
