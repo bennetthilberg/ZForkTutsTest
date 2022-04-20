b46da91ddbe8df10136ae45cab0ada04f1175801
Zhixing Jiang <jiangz@carleton.edu>
2022-04-19 23:06:18 -0500

    working tutorial with multiset

--- source/ITutorial.h
+++ source/ITutorial.h
@@ -8,7 +8,7 @@
 #include "../Empirical/include/emp/web/Button.hpp"
 #include "../Empirical/include/emp/web/Canvas.hpp"
 #include "../Empirical/include/emp/web/web.hpp"
-
+ 
 namespace UI = emp::web;
 class ITutorial {
     private:
@@ -17,10 +17,10 @@ class ITutorial {
         Tutorial tut;
         UI::Button ok_but;
         UI::Button start_but;
-
+ 
     public:
         static void PrintComplete() { std::cout << "Tutorial Complete!" << std::endl; }
-
+ 
         ITutorial(UI::Document animation, UI::Document settings, UI::Document explanation, UI::Document learnmore, UI::Document buttons, UI:: Document top_bar, UI::Canvas mycanvas,UI::Document instructions, UI::Button my_button): doc("emp_base"), ok_but([](){}, "OK"){
             // doc << my_button;
             // top_bar << my_button;
@@ -32,7 +32,7 @@ class ITutorial {
             // my_button.SetCSS("right", "23.5vw");
             // my_button.SetCSS("bottom", "1.5vh");
             ok_but.SetCSS("position", "relative");
-
+ 
             /* Add all states */
             tut.AddState("first_state");
             tut.AddState("start_but_state");
@@ -43,8 +43,7 @@ class ITutorial {
             tut.AddState("graph_state");
             tut.AddState("repeat_state");
             tut.AddState("end_state", &PrintComplete);
-            
-
+ 
             /* All triggers */
             tut.AddEventListenerTrigger("first_state", "start_but_state", my_button, "click", "click_trigger");
             tut.AddEventListenerTrigger("start_but_state", "reset_but_state", ok_but, "click", "click_ok");
@@ -54,69 +53,72 @@ class ITutorial {
             tut.AddExistingTrigger("lab_instruct_state", "graph_state", "click_ok");
             tut.AddExistingTrigger("graph_state", "repeat_state", "click_ok");
             tut.AddExistingTrigger("repeat_state", "end_state", "click_ok");
-            tut.AddExistingTrigger("end_state", "start_but_state", "click_trigger");
             ok_but.SetCSS("z-index", "13");
             /*start_but_state*/
-            tut.AddPopoverEffect("start_but_state", buttons, "Welcome to Symbulation, an interactive online lab that explores the relationships between hosts and symbionts. Before starting this tutorial, click the symbulation background button to learn about how the simulation runs. Once you have read it, click next.", ok_but, "2vh", "-23vw", "60%");
-            tut.AddPopoverEffect("start_but_state", buttons, "This grid represents a petri dish holding the hosts and symbionts in our environment. Each host is a square, and each symbiont is a circle. To start the simulation, just push start and observe as the petri dish starts to change. You can pause the simulation at any time with the pause button. ", ok_but, "2vh", "25vw", "auto");
+            tut.AddPopoverEffect("start_but_state", buttons, "This is a walk through of the UI", ok_but, "15vh", "-4vw");
+            tut.AddPopoverEffect("start_but_state", buttons, "Click it to start or pause the experiement", ok_but, "-3vh", "-4vw");
             tut.AddOverlayEffect("start_but_state", buttons, buttons, "black", 0.8, 10, true);
-
-
+            // emp::Ptr<OverlayEffect> overlayPtr = tut.AddOverlayEffect("start_but_state", buttons, "black", 0.8, 10, true);
+            // std::string curState = tut.GetCurrentState();
+            // std::cout << "Testing......current state: "<< curState<<std::endl;
+            // if (curState=="start_but_state") {
+            //     overlayPtr -> SetOverlayCSS("position", "relative");
+            //     overlayPtr -> SetOverlayCSS("z-index", "10");
+            //     overlayPtr -> SetOverlayCSS("background-color", "red");
+            // }
+ 
             /*reset_but_state*/
-            tut.AddPopoverEffect("reset_but_state", buttons, "To reset the simulation, push reset. You will need to click reset after you make changes to the settings before starting the simulation.", ok_but, "2vh", "0vw", "auto");
+            tut.AddPopoverEffect("reset_but_state", buttons, "Click it to reset petri dish", ok_but, "1vh", "0vw");
             tut.AddOverlayEffect("reset_but_state", buttons, settings, "black", 0.8, 10, true);
             /*settings_state*/
-            tut.AddPopoverEffect("settings_state", buttons, "These are the settings panels. Each setting has a dropdown that explains more about what that setting does. Try expanding a dropdown to learn more about a setting.", ok_but, "2vh", "-5vw", "90%");
+            tut.AddPopoverEffect("settings_state", buttons, "This is the settings panel. Try to fold and drop down", ok_but, "-7vh", "-27vw");
             tut.AddOverlayEffect("settings_state", buttons, buttons, "black", 0.8, 10, true);
-
+ 
             /*settings_change_state*/
-            tut.AddPopoverEffect("settings_change_state", buttons, "You can change a setting by adjusting the slider, typing in a value, or using the small arrows. Try changing a setting now!", ok_but, "2vh", "-5vw", "90%");
+            tut.AddPopoverEffect("settings_change_state", buttons, "Try to change the parameters and start a new experiment to see the difference", ok_but, "-7vh", "-27vw");
             tut.AddOverlayEffect("settings_change_state", buttons, settings, "black", 0.8, 10, true);
-            tut.AddPopoverEffect("settings_change_state", buttons, "After changing your settings, you must push reset to see your changes reflected in the petri dish. Try pushing reset, then starting the simulation again. Did you see the changes?", ok_but, "25vh", "-5vw", "90%");
-            
-
+ 
             /*lab_instruct_state*/
-            tut.AddPopoverEffect("lab_instruct_state", buttons, "Your lab instructions are held here. Each section can be collapsed to save space on your screen", ok_but, "2vh", "20vw", "auto");
+            tut.AddPopoverEffect("lab_instruct_state", buttons, "Drop this card down to see lab instructions", ok_but, "-7vh", "30vw");
             tut.AddOverlayEffect("lab_instruct_state", buttons, explanation, "black", 0.8, 10, true);
-
+ 
             /*graph_state*/
-            tut.AddPopoverEffect("graph_state", buttons, "Below the lab instructions are live graphs. Click the drop down to see them!", ok_but, "30vh", "20vw", "auto");
+            tut.AddPopoverEffect("graph_state", buttons, "Drop this card down to see live graphs of the experiment", ok_but, "1vh", "30vw");
             tut.AddOverlayEffect("graph_state", buttons, explanation, "black", 0.8, 10, true);
-
+ 
             /*repeat_state*/
-            tut.AddPopoverEffect("repeat_state", buttons, "Click reset and start on the lab to watch the graphs update in real time.", ok_but, "2vh", "30vw", "auto");
-            tut.AddOverlayEffect("repeat_state", buttons, explanation, "black", 0.8, 10, true);
-            
+ 
             /*end_state*/
-            tut.AddPopoverEffect("end_state", buttons, "You have finished the tutorial! To start it again at any time, click the start tutorial button. If you have any questions, click the FAQ button, or refer to the symbulation background.", ok_but, "2vh", "0vw", "60%");
-            tut.AddOverlayEffect("end_state", buttons, explanation, "black", 0.8, 10, true);
-            
-
+ 
             //tut.AddExistingTrigger("second_state", "third_state", "click_trigger");
             //tut.AddEventListenerTrigger("third_state", "end_state", ok_but, "click", "click_ok");
             tut.StartAtState("first_state");
-
+ 
         }
-
+ 
         void startTut(UI::Document animation, UI::Document settings, UI::Document explanation, UI::Document learnmore, UI::Document buttons, UI::Canvas mycanvas, UI::Document instructions) {
-
+ 
             ok_but.SetCSS("left", "0.7vw");
             instructions.SetCSS("z-index","0");
-            instructions.SetCSS("position", "relative");
+            instructions.SetCSS("position", "static");
             // animation.SetCSS("position", "relative");
             // animation.SetCSS("z-index", "12");
-            
+           
             tut.AddSpotlight("start_but_state", animation);
             tut.AddSpotlight("start_but_state", buttons.Button("toggle"));
             tut.AddSpotlight("reset_but_state", buttons.Button("reset"));
-            tut.AddSpotlight("settings_state", settings);
+            tut.AddSpotlight("settings_state", settings.Div("emp__449_MAIN_outer_card_header"));
+            tut.AddSpotlight("settings_state", settings.Div("emp__449_HOST_outer_card_header"));
+            tut.AddSpotlight("settings_state", settings.Div("emp__449_SYM_outer_card_header"));
+            tut.AddSpotlight("settings_state", settings.Div("emp__449_ADV_outer_card_header"));
             tut.AddSpotlight("settings_change_state", settings);
-            
-
-
+           
+ 
+ 
             // tut.StartAtState("first_state");
-                
-
+               
+ 
         }
 };
 #endif
+
--- source/SymAnimate.h
+++ source/SymAnimate.h
@@ -129,10 +129,10 @@ public:
 
     top_bar << "<div class=\"rightB\">";
     top_bar << "<button class=\"test1\" onclick = 'f=window.open(\"FAQ.html\",\"fenetre\",\"the style (without style tag, example - width=400, height=600, no px\")'style=\"cursor: pointer;\">FAQ</button>";
+    top_bar << start_tutorial;
     top_bar << "<button class=\"test1\" onclick = 'f=window.open(\"biology_background.html\",\"fenetre\",\"the style (without style tag, example - width=400, height=600, no px\")'style=\"cursor: pointer;\">Biology Background</button>";
     top_bar << "<button class=\"test1\" onclick = 'f=window.open(\"symb_overview.html\",\"fenetre\",\"the style (without style tag, example - width=400, height=600, no px\")'style=\"cursor: pointer;\">Symbulation Overview</button>";
     top_bar << "<a href=\"https://anyaevostinar.github.io/SymbulationEmp/web/symbulation.html\" ><button class=\"test1\">Home GUI</button></a>";
-    top_bar << start_tutorial;
     top_bar << "</div>";
 
     initializeWorld();
@@ -223,17 +223,7 @@ public:
     //  data-container="body" data-toggle="popover" data-placement="top" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">)
     
     // ----------------------- Keep track of number of updates -----------------------
-    buttons << "<br>";
-    buttons << UI::Text("update") << "Update = " << UI::Live( [this](){ return world.GetUpdate(); } ) << "  ";
-    buttons << "<br>";
 
-    // Add a canvas for petri dish and draw the initial petri dish
-    mycanvas = animation.AddCanvas(RECT_WIDTH*config.GRID_X(), RECT_WIDTH*config.GRID_Y(), "can");
-    targets.push_back(mycanvas);
-    drawPetriDish(mycanvas);
-    animation << "<br>";
-
-    itut.startTut(animation, settings, explanation, learnmore, buttons, mycanvas, instructions);
 
     emp::prefab::Card graphs_card(false ? "INIT_OPEN" : "INIT_CLOSED", true, "graphs_card");
     graphs_card.AddHeaderContent("Data Collection");
@@ -271,8 +261,18 @@ public:
     emp::prefab::Card card_instructions(true ? "INIT_OPEN" : "INIT_CLOSED", true, "instructions_card");
     initializeInstructionsCard(card_instructions);
     instructions << card_instructions;
+
+
+    buttons << "<br>";
+    buttons << UI::Text("update") << "Update = " << UI::Live( [this](){ return world.GetUpdate(); } ) << "  ";
+    buttons << "<br>";
+
+    // Add a canvas for petri dish and draw the initial petri dish
+    mycanvas = animation.AddCanvas(RECT_WIDTH*config.GRID_X(), RECT_WIDTH*config.GRID_Y(), "can");
+    targets.push_back(mycanvas);
+    drawPetriDish(mycanvas);
+    animation << "<br>";
     itut.startTut(animation, settings, explanation, learnmore, buttons, mycanvas, instructions);
-    
   }
 
   void initializeInstructionsCard(emp::prefab::Card & card){
