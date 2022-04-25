98ad1e04aae493bb7d9d2b4bde19d21cf8b3c9e4
Chloe Morscheck <chloemorscheck@Chloes-MacBook-Pro.local>
2022-04-25 18:27:40 -0500

    made tut prettier

--- source/ITutorial.h
+++ source/ITutorial.h
@@ -21,16 +21,10 @@ class ITutorial {
         static void PrintComplete() { std::cout << "Tutorial Complete!" << std::endl; }
  
         ITutorial(UI::Document & animation, UI::Document & settings, UI::Document & explanation, UI::Document & learnmore, UI::Document & buttons, UI::Document & top_bar, UI::Canvas & mycanvas,UI::Document & instructions, UI::Button & my_button): doc("emp_base"), ok_but([](){}, "OK"){
-            // doc << my_button;
-            // top_bar << my_button;
-            // my_button.OnMouseOver([this](){auto but  =my_button; but.SetCSS("background-color", "#3d1477"); but.SetCSS("cursor", "pointer"); but.SetCSS("color", "white");});
-            // my_button.SetAttr("class", "btn btn-secondary");
-            // my_button.OnMouseOut([this](){auto but=my_button; but.SetCSS("background-color", "#5f8eff"); but.SetCSS("color", "white");});
-            // my_button.SetCSS("background-color", "#5f8eff");
-            // my_button.SetCSS("position", "absolute");
-            // my_button.SetCSS("right", "23.5vw");
-            // my_button.SetCSS("bottom", "1.5vh");
+            
             ok_but.SetCSS("position", "relative");
+            ok_but.SetCSS("z-index", "13");
+            my_button.SetCSS("z-index", "13");
  
             /* Add all states */
             tut.AddState("first_state");
@@ -71,14 +65,11 @@ class ITutorial {
             tut.AddExistingTrigger("graph_state", "end_state", "click_trigger");
             tut.AddExistingTrigger("repeat_state", "end_state", "click_trigger");
 
-
-            ok_but.SetCSS("z-index", "13");
-            my_button.SetCSS("z-index", "13");
             /*start_but_state*/
-            tut.AddPopoverEffect("intro_state", buttons, "Welcome to Symbulation, an interactive online lab that explores the relationships between hosts and symbionts. Before starting this tutorial, click the symbulation background button to learn about how the simulation runs. Once you have read it, click ok.", ok_but, "15vh", "-4vw", "auto");
+            tut.AddPopoverEffect("intro_state", buttons, "Welcome to Symbulation, an interactive online lab that explores the relationships between hosts and symbionts. Before starting this tutorial, click the symbulation background button to learn about how the simulation runs. Once you have read it, click ok.", ok_but, "1vh", "-4vw", "auto");
             tut.AddOverlayEffect("intro_state", buttons, buttons, "black", 0.8, 10, true);
             
-            tut.AddPopoverEffect("start_but_state", buttons, "This grid represents a petri dish holding the hosts and symbionts in our environment. Each host is a square, and each symbiont is a circle. To start the simulation, just push start and observe as the petri dish starts to change. You can pause the simulation at any time with the pause button.", ok_but, "-3vh", "-4vw", "auto");
+            tut.AddPopoverEffect("start_but_state", buttons, "This grid represents a petri dish holding the hosts and symbionts in our environment. Each host is a square, and each symbiont is a circle. To start the simulation, just push start and observe as the petri dish starts to change. You can pause the simulation at any time with the pause button.", ok_but, "-10vh", "-4vw", "auto");
             tut.AddOverlayEffect("start_but_state", buttons, buttons, "black", 0.8, 10, true);
             // emp::Ptr<OverlayEffect> overlayPtr = tut.AddOverlayEffect("start_but_state", buttons, "black", 0.8, 10, true);
             // std::string curState = tut.GetCurrentState();
@@ -90,31 +81,31 @@ class ITutorial {
             // }
  
             /*reset_but_state*/
-            tut.AddPopoverEffect("reset_but_state", buttons, "To reset the simulation, push reset. You will need to click reset after you make changes to the settings before starting the simulation.", ok_but, "1vh", "0vw", "auto");
+            tut.AddPopoverEffect("reset_but_state", buttons, "To reset the simulation, push reset. You will need to click reset after you make changes to the settings before starting the simulation.", ok_but, "1vh", "-5vw", "auto");
             tut.AddOverlayEffect("reset_but_state", buttons, settings, "black", 0.8, 10, true);
             /*settings_state*/
-            tut.AddPopoverEffect("settings_state", buttons, "These are the settings panels. Each setting has a dropdown that explains more about what that setting does. Try expanding a dropdown to learn more about a setting. ", ok_but, "-7vh", "-27vw", "auto");
+            tut.AddPopoverEffect("settings_state", buttons, "These are the settings panels. Each setting has a dropdown that explains more about what that setting does. Try expanding a dropdown to learn more about a setting. ", ok_but, "1vh", "-5vw", "auto");
             tut.AddOverlayEffect("settings_state", buttons, buttons, "black", 0.8, 10, true);
  
             /*settings_change_state*/
-            tut.AddPopoverEffect("settings_change_state", buttons, "You can change a setting by adjusting the slider, typing in a value, or using the small arrows. Try changing a setting now!", ok_but, "-7vh", "-27vw", "auto");
+            tut.AddPopoverEffect("settings_change_state", buttons, "You can change a setting by adjusting the slider, typing in a value, or using the small arrows. Try changing a setting now!", ok_but, "1vh", "-5vw", "auto");
             tut.AddOverlayEffect("settings_change_state", buttons, settings, "black", 0.8, 10, true);
  
             /*reset_change_state*/
-            tut.AddPopoverEffect("reset_change_state", buttons, "After changing your settings, you must push reset to see your changes reflected in the petri dish. Try pushing reset, then starting the simulation again. Did you see the changes?", ok_but, "-7vh", "-27vw", "auto");
+            tut.AddPopoverEffect("reset_change_state", buttons, "After changing your settings, you must push reset to see your changes reflected in the petri dish. Try pushing reset, then starting the simulation again. Did you see the changes?", ok_but, "-5vh", "-4vw", "auto");
             tut.AddOverlayEffect("reset_change_state", buttons, settings, "black", 0.8, 10, true);
             
             /*lab_instruct_state*/
-            tut.AddPopoverEffect("lab_instruct_state", buttons, "Your lab instructions are held here. Each section can be collapsed to save space on your screen", ok_but, "-7vh", "30vw", "auto");
+            tut.AddPopoverEffect("lab_instruct_state", buttons, "Your lab instructions are held here. Each section can be collapsed to save space on your screen", ok_but, "1vh", "10vw", "70%");
             tut.AddOverlayEffect("lab_instruct_state", buttons, explanation, "black", 0.8, 10, true);
  
             /*graph_state*/
-            tut.AddPopoverEffect("graph_state", buttons, "Below the lab instructions are live graphs. Click the drop down to see them!", ok_but, "1vh", "30vw", "auto");
-            tut.AddPopoverEffect("graph_state", buttons, "Click reset and start on the lab to watch the graphs update in real time. ", ok_but, "1vh", "30vw", "auto");
+            tut.AddPopoverEffect("graph_state", buttons, "Below the lab instructions are live graphs. Click the drop down to see them!", ok_but, "20vh", "25vw", "40%");
+            tut.AddPopoverEffect("graph_state", buttons, "Click reset and start on the lab to watch the graphs update in real time. ", ok_but, "30vh", "25vw", "40%");
             tut.AddOverlayEffect("graph_state", buttons, explanation, "black", 0.8, 10, true);
  
             /*repeat_state*/
-            tut.AddPopoverEffect("repeat_state", buttons, "You have finished the tutorial! To start it again at any time, click the start tutorial button. If you have any questions, click the FAQ button, or refer to the symbulation background. ", ok_but, "1vh", "30vw", "auto");
+            tut.AddPopoverEffect("repeat_state", buttons, "You have finished the tutorial! To start it again at any time, click the start tutorial button. If you have any questions, click the FAQ button, or refer to the symbulation background. ", ok_but, "1vh", "-4vw", "auto");
             tut.AddOverlayEffect("repeat_state", buttons, explanation, "black", 0.8, 10, true);
  
             /*end_state*/
@@ -134,13 +125,15 @@ class ITutorial {
             tut.AddSpotlight("start_but_state", animation);
             tut.AddSpotlight("start_but_state", buttons.Button("toggle"));
             tut.AddSpotlight("reset_but_state", buttons.Button("reset"));
-            tut.AddSpotlight("settings_state", settings.Div("emp__449_MAIN_outer_card_header"));
-            tut.AddSpotlight("settings_state", settings.Div("emp__449_HOST_outer_card_header"));
-            tut.AddSpotlight("settings_state", settings.Div("emp__449_SYM_outer_card_header"));
-            tut.AddSpotlight("settings_state", settings.Div("emp__449_ADV_outer_card_header"));
+            //tut.AddSpotlight("settings_state", settings.Div("emp__449_MAIN_outer_card_header"));
+            //tut.AddSpotlight("settings_state", settings.Div("emp__449_HOST_outer_card_header"));
+            //tut.AddSpotlight("settings_state", settings.Div("emp__449_SYM_outer_card_header"));
+            //tut.AddSpotlight("settings_state", settings.Div("emp__449_ADV_outer_card_header"));
             tut.AddSpotlight("settings_state", settings);
             tut.AddSpotlight("settings_change_state", settings);
             tut.AddSpotlight("intro_state", top_bar);
+            tut.AddSpotlight("graph_state", buttons);
+            tut.AddSpotlight("reset_change_state", buttons);
            
  
  
